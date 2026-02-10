0d8ab0d1b4edf7a6a9929c7ef6314db01a08de3e
Riya Devkota <riyadevkota2062@gmail.com>
2026-02-10 18:25:02 +0545

    Refactor main method to call methody directly and remove unused display method

--- INHERITANCE/super_3.java
+++ INHERITANCE/super_3.java
@@ -4,13 +4,10 @@ public class super_3 {
     //methods invoking
     public static void main(String[]args){
 classy2 c=new classy2();
-c.display();
+c.methody();
     }
 }
 class classsy{
-    String name;
-    int age;
-
 
     void methody(){
 System.out.println("This is method1");
@@ -18,11 +15,10 @@ System.out.println("This is method1");
 }
 class classy2 extends classsy{
     void methody(){
+        super.methody();
 System.out.println("This is method 2");
     }
 
-     void display(){
-        methody();
-        super.methody();
-    }
+
+     
 }
