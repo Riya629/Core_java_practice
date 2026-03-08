246a97926f2f98dfbb47b9b6686a19546d81d2f3
Riya Devkota <riyadevkota2062@gmail.com>
2026-03-08 20:58:09 +0545

    Add Assign_Abstract_2 class with implementation of abstract pen and fountain pen

---
+++ ABSTRACT/Assign_Abstract_2.java
@@ -0,0 +1,25 @@
+package home.ABSTRACT;
+
+public class Assign_Abstract_2 {
+    public static void main(String[]args){
+fountainpen fp = new fountainpen();
+fp.changenib();
+fp.write();
+fp.refill();
+    }
+}
+ abstract class pen2{
+    abstract void write();
+    abstract void refill();
+}
+class fountainpen extends pen2{
+    void write(){
+        System.out.println("The pen writes smootly");
+    }
+    void refill(){
+        System.out.println("we can refill this pen");
+    }
+    void changenib(){
+        System.out.println("change the nib of pen");
+    }
+}
